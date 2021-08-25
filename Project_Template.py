d89ad32a5b7206f9614144d4e4a792bc7620744b
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-25 09:25:11 +0530

    Create Project_Template.py

---
+++ Project_Template.py
@@ -0,0 +1,12 @@
+#Import time module
+
+numbers=[0,1,5,19,65,211,"?"]
+#Hint: 3^n-2^n=665
+#Make first timepoint
+
+ans=int(input("Enter the missing number: "))
+print(ans)
+while ans!=665:
+  ans=input("Enter the missing number again: ")
+#Check if answer is correct and calculate total time
+    
