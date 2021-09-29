f9a4c72d6ff2dd61ca01672d252f0052f2c0a887
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-09-29 08:46:26 +0530

    Update Project_Template.py

--- Project_Template.py
+++ Project_Template.py
@@ -2,6 +2,7 @@
 import 
 
 numbers=[0,1,5,19,65,211,"?"]
+print(numbers)
 #Hint: 3^n-2^n = 665
 # Create the first timepoint 't1'
 t1 = 
@@ -9,7 +10,7 @@ t1 =
 ans=int(input("Enter the missing number: "))
 print(ans)
 while ans!=665:
-  ans=input("Enter the missing number again: ")
+  ans=int(input("Enter the missing number again: "))
   
 # Check if answer is equal to 665
 if ans == 665:
